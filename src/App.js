1be463991494ade3110dbbadace51a279fd350a3
taejin <taejin5314@gmail.com>
2021-05-13 17:36:32 -0400

    update login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
       <h2>Let's build the linkedin clone!</h2>
+      <Login></Login>
     </div>
   );
 }
