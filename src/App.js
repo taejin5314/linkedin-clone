9e5a96b54cf67bcb98f2acde80d23ef0aa2e5181
taejin <taejin5314@gmail.com>
2021-05-13 17:46:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <h2>Let's build the linkedin clone!</h2>
-      <Login></Login>
+      <Router>
+        <Switch>
+          <Route exact path='/'></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
