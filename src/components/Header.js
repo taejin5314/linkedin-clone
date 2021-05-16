dc345ad09fca1b5e3ff5108e5fe4faa0a2f74b1f
taejin <taejin5314@gmail.com>
2021-05-16 19:01:45 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -115,7 +115,7 @@ const NavListWrap = styled.ul`
 const NavList = styled.li`
   display: flex;
   align-items: center;
-  & a {
+  & > a {
     align-items: center;
     background: transparent;
     display: flex;
@@ -124,12 +124,12 @@ const NavList = styled.li`
     font-weight: 400;
     justify-content: center;
     line-height: 1.5;
-    min-height: 42px;
+    min-height: 52px;
     min-width: 80px;
     position: relative;
     text-decoration: none;
 
-    & span {
+    & > span {
       color: rgba(0, 0, 0, 0.6);
       display: flex;
       align-items: center;
@@ -137,6 +137,15 @@ const NavList = styled.li`
 
     @media (max-width: 768px) {
       min-width: 70px;
+    }
+  }
+
+  &:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
     }
   }
 `;
