256e142bc0d9b88bbb2a3549c3a0a0be6613f400
taejin <taejin5314@gmail.com>
2021-05-17 17:20:03 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,11 @@ const Home = (props) => {
         <h5><a>Hiring in a hurry? -</a></h5>
         <p> Find talented pros in record time with Upwork and keep business moving.</p>
       </Section>
+      <Layout>
+        <div>Left Side</div>
+        <div>Main</div>
+        <div>Right Side</div>
+      </Layout>
     </Container>
   )
 };
@@ -48,6 +53,12 @@ const Section = styled.section`
     flex-direction: column;
     padding: 0 5px;
   }
+`;
+
+const Layout = styled.div`
+  display: grid;
+  grid-template-areas: "leftside main rightside";
+  grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
 `;
 
 export default Home;
