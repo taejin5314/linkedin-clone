4e840723f35d1c507a2820017bb11bfa16dcc122
taejin <taejin5314@gmail.com>
2021-05-17 16:36:33 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import styled from 'styled-components';
 
 const Home = (props) => {
-  return <div>Home</div>
+  return (
+    <Container>
+      <Section>
+        <h5><a>Hiring in a hurry? - </a></h5>
+        <p>Find talented pros in record time with Upwork and keep business moving.</p>
+      </Section>
+    </Container>
+  )
 };
+
+const Container = styled.div``;
+
+const Content = styled.div``;
+
+const Section = styled.section``;
 
 export default Home;
