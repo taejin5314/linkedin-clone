efe32e0aa63253d37db171b691b5f809baa1b49f
taejin <taejin5314@gmail.com>
2021-05-17 16:29:05 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -61,6 +61,10 @@ const Header = (props) => {
                   <img src="/images/down-icon.svg" alt="" />
                 </span>
               </a>
+
+              <SignOut>
+                <a>Sign out</a>
+              </SignOut>
             </User>
 
             <Work>
@@ -210,6 +214,19 @@ const NavList = styled.li`
       }
     }
   }
+  `;
+
+const SignOut = styled.div`
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
 `;
 
 const User = styled(NavList)`
@@ -223,10 +240,19 @@ const User = styled(NavList)`
     display: flex;
     align-items: center;
   }
+
+  &:hover {
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
 `;
 
 const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
+
 
 export default Header;
