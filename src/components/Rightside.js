0aec5fdf680a23eb17dd54fdc0c9c9a9a6698bf2
taejin <taejin5314@gmail.com>
2021-05-18 16:49:13 -0400

    right side card

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -3,7 +3,11 @@ import styled from 'styled-components';
 const Rightside = (props) => {
   return (
     <Container>
-      <FollowCard>rightside</FollowCard>
+      <FollowCard>
+        <Title>
+          <h2>Add to your feed</h2>
+        </Title>
+      </FollowCard>
     </Container>
   );
 }
@@ -22,6 +26,15 @@ const FollowCard = styled.div`
   border: none;
   box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 / 20%);
   padding: 12px;
+`;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
 `;
 
 export default Rightside;
