ca459c71d40373399e9d7657f8d9e1657d071328
taejin <taejin5314@gmail.com>
2021-05-18 16:34:20 -0400

    Update Rightside.js

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -19,6 +19,9 @@ const FollowCard = styled.div`
   background-color: #fff;
   border-radius: 5px;
   position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 / 20%);
+  padding: 12px;
 `;
 
 export default Rightside;
