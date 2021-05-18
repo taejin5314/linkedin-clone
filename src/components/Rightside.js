97e8fe372a491ad3b9ad72929bafe2c859a3cb4a
taejin <taejin5314@gmail.com>
2021-05-18 16:56:06 -0400

    Update Rightside.js

--- src/components/Rightside.js
+++ src/components/Rightside.js
@@ -60,7 +60,39 @@ const Title = styled.div`
   color: rgba(0, 0, 0, 0.6);
 `;
 
-const FeedList = styled.ul``;
+const FeedList = styled.ul`
+  margin-top: 16px;
+
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
+    position: relative;
+    font-size: 14px;
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+
+    button {
+      background-color: transparent;
+      color: rgba(0, 0, 0, 0.6);
+      box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      align-items: center;
+      border-radius: 15px;
+      box-sizing: border-box;
+      font-weight: 600;
+      display: inline-flex;
+      justify-content: center;
+      max-height: 32px;
+      max-width: 480px;
+      text-align: center;
+      outline: none;
+    }
+  }
+`;
 const Avatar = styled.div``;
 
 export default Rightside;
