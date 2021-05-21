10fb9bd157ea9887aeb0bedd767ef8faafc70988
taejin <taejin5314@gmail.com>
2021-05-21 19:58:16 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -56,7 +56,11 @@ const Header = (props) => {
 
             <User>
               <a>
-                <img src="/images/user.svg" alt="" />
+                {
+                  props.user && props.user.photoURL ?
+                    (<img src={props.user.photoURL} alt="" />)
+                    : (<img src="/images/user.svg" alt="" />)
+                }
                 <span>Me
                   <img src="/images/down-icon.svg" alt="" />
                 </span>
