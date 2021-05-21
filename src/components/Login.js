74cfa90f65f2d09ffdad6883b8bcf703bce63156
taejin <taejin5314@gmail.com>
2021-05-21 18:10:20 -0400

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import { signInAPI } from '../actions'
 
 const Login = (props) => {
   return (
@@ -19,7 +20,7 @@ const Login = (props) => {
           <img src="/images/login-hero.svg" alt="" />
         </Hero>
         <Form>
-          <Google>
+          <Google onClick={() => props.signIn()}>
             <img src="/images/google.svg" alt="" />
             Sign in with Google
           </Google>
@@ -174,7 +175,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = (dispatch) => ({
-
+  signIn: () => dispatch(signInAPI()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
