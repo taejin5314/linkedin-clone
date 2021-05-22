6e8868dfcbab087b24c40db447c58b7381631002
taejin <taejin5314@gmail.com>
2021-05-22 14:09:26 -0400

    Create PostModal.js

---
+++ src/components/PostModal.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const PostModal = (props) => {
+  return (
+    <Container>
+      PostModal
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  position: fixed;
+`;
+
+export default PostModal;
