610e7954afb0247704f80e25b1bb46f464509f5a
taejin <taejin5314@gmail.com>
2021-05-24 17:43:53 -0400

    add reducer

---
+++ src/reducers/articleReducer.js
@@ -0,0 +1,12 @@
+export const initState = {
+  loading: false,
+};
+
+const aritcleReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default aritcleReducer;
